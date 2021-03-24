ac5d50103220be5cac6fe550dce7ad41b7c88661
Emanuel-developer992 <Orion_Developer@outlook.com>
2021-03-24 15:58:58 -0300

    1.1v

--- forms/Cadastro de Cliente-Patrocinador/cepAuto.js
+++ forms/Cadastro de Cliente-Patrocinador/cepAuto.js
@@ -1,11 +1,14 @@
-$("#cep").blur(function() {
+$("#cep").blur(function(blur) {
+
+    var pais = "Brasil";
     
+
     $.getJSON("//viacep.com.br/ws/"+ $("#cep").val() +"/json/", function(dados){ 
         
         $("#endereco").val(dados.logradouro);
         $("#bairro").val(dados.bairro);
         $("#estado").val(dados.localidade);
-        $("#pais").val(dados.pais);
+        
         
         
     })
