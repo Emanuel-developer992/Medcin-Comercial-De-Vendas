82f474f88f57e80a69b30f255ca60833ce8c28de
Emanuel-developer992 <Orion_Developer@outlook.com>
2021-03-24 20:45:38 -0300

    1.2v

---
+++ forms/Cadastro Master/cardsCadastro.js
@@ -0,0 +1,27 @@
+function cadastroCP() {
+
+    const clienteP = "Cadastro de Cliente/Patrocinador";
+    const cadastroCard = document.getElementById("cadastroCard");
+
+    cadastroCard.value = clienteP;
+
+
+}
+function cadastroPS() {
+
+    const produtosS = "Cadastro de Produtos e Serviços";
+    const cadastroCard = document.getElementById("cadastroCard");
+
+    cadastroCard.value = produtosS;
+
+
+}
+function cadastroO() {
+
+    const orcamento = "Cadastro de Orçamento";
+    const cadastroCard = document.getElementById("cadastroCard");
+
+    cadastroCard.value = orcamento;
+
+
+}
